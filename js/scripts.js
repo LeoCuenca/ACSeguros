b948700827dd875da365a61eb613367b3b21588a
Leonel Cuenca <leo.cuenca.hw@gmail.com>
2024-02-26 14:54:40 -0300

    Adaptacion Mobile

--- js/scripts.js
+++ js/scripts.js
@@ -34,58 +34,54 @@ $(function() {
 
 // MARCA DACADA BOTON A MEDIDA QUE ME DESPLAZO
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+document.addEventListener("DOMContentLoaded", function() {
 
-window.onscroll = () => {
-  sections.forEach(sec => {
-      let top = window.scrollY;
-      let offset = sec.offsetTop - 150;
-      let height = sec.offsetHeight;
-      let id = sec.getAttribute('id');
+  var contenedor = document.querySelector('.page-content');
+  var contenedorHeight = contenedor.offsetHeight;
 
-      if(top >= offset && top < offset + height){
-          navLinks.forEach(links => {
-              links.classList.remove('active');
-              document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+  var cards = document.querySelectorAll('.card');
+  var card = document.querySelector('.card');
+  var cardHeight = card.offsetHeight;
+
+  var descripciones = document.querySelectorAll('.descripcion');
+
+  cards.forEach(function(card, index) {
+      card.addEventListener('click', function() {
+          // Remover la clase 'active' de todas las tarjetas
+          cards.forEach(function(card) {
+              card.classList.remove('active');
           });
-      }
-  });
-}
+          
+          // Agregar la clase 'active' a la tarjeta clickeada
+          cards[index].classList.add('active');
 
-// SHOW HIDE SEGUROS DESCRIPTION
+          // Verificar si la descripción correspondiente a la card clickeada está visible
+          if (descripciones[index].classList.contains('visible')) {
+              // Si está visible, ocultarla
+              cards[index].classList.remove('active');
 
-var cards = document.querySelectorAll('.card');
-var contenedor = document.querySelector('.page-content');
-var descripciones = document.querySelectorAll('.descripcion');
+              contenedor.style.height = contenedorHeight +'px';
 
-cards.forEach(function(card, index) {
-    card.addEventListener('click', function() {
-        // Remover la clase 'active' de todas las tarjetas
-        cards.forEach(function(card) {
-            card.classList.remove('active');
-        });
+              descripciones[index].classList.remove('visible');
+          } else {
+                  // Si no está visible, ocultar todas las descripciones y luego mostrar la descripción correspondiente a la card clickeada
+                  descripciones.forEach(function(descripcion) {
+                  descripcion.classList.remove('visible');
+              });
+              if (window.innerWidth > 1024){
+                console.log('MAYOR A 1024');
+                contenedor.style.height = '46rem';
+              }
+              else{
+                console.log("MENOR A 1024");
+                contenedor.style.height = contenedorHeight + 450 + 'px';
+              }
 
-        // Agregar la clase 'active' a la tarjeta clickeada
-        cards[index].classList.add('active');
-
-        // Verificar si la descripción correspondiente a la card clickeada está visible
-        if (descripciones[index].classList.contains('visible')) {
-            // Si está visible, ocultarla
-            cards[index].classList.remove('active');
-            contenedor.style.height = '20rem'; // Altura mínima
-            descripciones[index].classList.remove('visible');
-        } else {
-                // Si no está visible, ocultar todas las descripciones y luego mostrar la descripción correspondiente a la card clickeada
-                descripciones.forEach(function(descripcion) {
-                descripcion.classList.remove('visible');
-            });
-            contenedor.style.height = '45rem'; // Altura máxima
-            descripciones[index].classList.add('visible');
-        }
+              descripciones[index].classList.add('visible');
+          }
     });
+  });
 });
-
 // CONTACT
 
 const inputs = document.querySelectorAll(".input");
@@ -107,3 +103,56 @@ inputs.forEach((input) => {
   input.addEventListener("blur", blurFunc);
 });
 
+/*==================== Typed JS ====================*/
+
+const typed = new Typed('.multiple-text',{
+  strings: ['Servicio al cliente', 'Seguridad y Transparencia'],
+  startDelay: 200,
+  typeSpeed: 25,
+  backSpeed: 50,
+  backDelay: 3000,
+  loop: true
+});
+
+const typedContact = new Typed('.multiple-contact',{
+  strings: ['Dejanos tu mensaje'],
+  typeSpeed: 100,
+  loop: false
+});
+
+/*==================== Scroll Reveal ====================*/
+
+ScrollReveal().reveal('.home-text p', { origin: 'left', reset:true, distance:'120px', delay:100, duration:2000 });
+
+
+
+
+
+
+
+ScrollReveal().reveal('.card-box .big-cont', { origin: 'top', reset:true, distance:'20px', delay:100, duration:1000 });
+ScrollReveal().reveal('.card-box .subtitle', { origin: 'bottom', reset:true, distance:'40px', delay:100, duration:1000 });
+
+
+
+
+
+
+ScrollReveal().reveal('.page-content .card:nth-child(1)', { origin: 'top', reset:true, distance:'50px', delay:200, duration:1000});
+ScrollReveal().reveal('.page-content .card:nth-child(2)', { origin: 'top', reset:true, distance:'50px', delay:400, duration:1000 });
+ScrollReveal().reveal('.page-content .card:nth-child(3)', { origin: 'top', reset:true, distance:'50px', delay:600, duration:1000 });
+ScrollReveal().reveal('.page-content .card:nth-child(4)', { origin: 'top', reset:true, distance:'50px', delay:800, duration:1000 });
+ScrollReveal().reveal('.page-content .card:nth-child(5)', { origin: 'top', reset:true, distance:'50px', delay:1000, duration:1000 });
+
+
+
+
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(1)', { origin: 'top', reset:true, distance:'100px', delay:200, duration:500});
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(2)', { origin: 'bottom', reset:true, distance:'100px', delay:200, duration:500});
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(3)', { origin: 'top', reset:true, distance:'100px', delay:200, duration:500});
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(4)', { origin: 'bottom', reset:true, distance:'100px', delay:200, duration:500});
+
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(5)', { origin: 'bottom', reset:true, distance:'100px', delay:200, duration:500});
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(6)', { origin: 'top', reset:true, distance:'100px', delay:200, duration:500});
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(7)', { origin: 'bottom', reset:true, distance:'100px', delay:200, duration:500});
+ScrollReveal().reveal('.accidents-container .accidents-box:nth-child(8)', { origin: 'top', reset:true, distance:'100px', delay:200, duration:500});
